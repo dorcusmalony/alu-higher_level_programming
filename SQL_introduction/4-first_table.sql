a93eb2f945a15ecb6fa51d48d82142d99c378f19
Dorcusmalony <d.malony@alustudent.com>
2023-08-03 03:17:24 -0700

    updated

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,29 @@
+-- creates a table called first_table in the current database
+CREATE TABLE IF NOT EXISTS first_table(
+    id INT,
+    name VARCHAR(256)
+);-- creates a table called first_table in the current database
+CREATE TABLE IF NOT EXISTS first_table(
+	    id INT,
+	    name VARCHAR(256)
+	);-- creates a table called first_table in the current database
+	CREATE TABLE IF NOT EXISTS first_table(
+		    id INT,
+		    name VARCHAR(256)
+		);-- creates a table called first_table in the current database
+		CREATE TABLE IF NOT EXISTS first_table(
+			    id INT,
+			    name VARCHAR(256)
+			);-- creates a table called first_table in the current database
+			CREATE TABLE IF NOT EXISTS first_table(
+				    id INT,
+				    name VARCHAR(256)
+				);-- creates a table called first_table in the current database
+				CREATE TABLE IF NOT EXISTS first_table(
+					    id INT,
+					    name VARCHAR(256)
+					);-- creates a table called first_table in the current database
+					CREATE TABLE IF NOT EXISTS first_table(
+						    id INT,
+						    name VARCHAR(256)
+						);
