351cb63683c9a33a6af6940746882bf8b74241a0
Dorcusmalony <d.malony@alustudent.com>
2023-08-17 11:41:26 -0700

    updated

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-'''a script 14-model_city_fetch_by_state.py that prints
-all City objects from the database hbtn_0e_14_usa'''
-
-
+""" Define State model """
+from model_state import Base
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
-Base = declarative_base()
+from sqlalchemy import Column, String, Integer, ForeignKey
 
 
 class City(Base):
-    '''class defination'''
+    """ Define a class State to be linked to db table """
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, nullable=False,
+                autoincrement=True, unique=True,
+                primary_key=True)
+
     name = Column(String(128), nullable=False)
+
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
