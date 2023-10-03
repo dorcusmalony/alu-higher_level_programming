957ac64fc9d8eb6e8f905ab09c1a5f69d789a5b9
dorcusmalony <d.malony@alustudent.com>
2023-10-03 12:50:11 -0700

    updated

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,17 +1,5 @@
 #!/usr/bin/node
-// a script that computes the number of tasks completed by user id
+// a script that gets the contents of a webpage and stores it in a file
+const fs = require('fs');
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    const completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
-  }
-});
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
